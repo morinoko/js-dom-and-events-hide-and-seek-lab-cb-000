1468b457ba9948035eb9e6305f8fe1087c569edd
Felice Forby <feliceforby@gmail.com>
2019-02-14 22:39:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.querySelector('#nested .target');
 }
+
+function increaseRankBy(n) {
+  const listItems = document.querySelector('.ranked-list li');
+
+  for (let i = 0, i < listItems.length, i++) {
+    let itemNumber = listItems[i].textContent.parseInt();
+    listItems[i].textContent = itemNumber + n;
+  }
+}
