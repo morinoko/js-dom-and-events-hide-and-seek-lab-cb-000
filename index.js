9b25513c3b56218a3cd6ecc28c50c2223344f638
Felice Forby <feliceforby@gmail.com>
2019-02-14 22:24:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function getFirstSelector(selector) {
   return document.querySelector(selector);
 }
+
+function nestedTarget(parentId, target) {
+  const parent = document.querySelector(parentId);
+
+  return parent.querySelector(target);
+}
